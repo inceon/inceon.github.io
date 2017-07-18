8c9e16b8be223d30e826aa1607276440ed812c6f
inceon <mr.inceon@gmail.com>
2017-07-18 23:30:04 +0300

    ftp deploy 3

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-abort() {
-    local message=$1
-    echo $message
-    exit -1
-}
-[ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
-[ -z $FTP_USER ] && abort "FTP_USER is undefined"
-[ -z $FTP_HOST ] && abort "FTP_HOST is undefined"
-lftp -u $FTP_USER,$FTP_PASS $FTP_HOST \
- -e 'mirror -c -e -R dist public_html ; exit'
+ lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R dist ~/public_html/"
