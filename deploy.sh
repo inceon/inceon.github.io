2fea2d50d9454e2e4f2a1534deb03149c51a0ada
inceon <mr.inceon@gmail.com>
2017-07-19 10:28:02 +0300

    fix build

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
- lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R dist ~/public_html/"
+sudo apt-get install lftp
+lftp -c "open -u $FTP_USER,$FTP_PASS $FTP_HOST; set ssl:verify-certificate no; mirror -R dist ~/public_html/"
